7f4e010bbca616bc396adece2a3c63734d726162
Chris Arter <carter@AdministorsMBP3.home>
2019-11-12 10:31:18 -0500

    formatting

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import serverless from 'serverless-http';
 const app: Express = express();
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.send("Hello World!");
+    res.send('Hello World!');
 });
 const handler = serverless(app);
 export { handler }
